53f98343a6ed17d7a9ef837c6c7b1bcbb992a625
Alex Bowers <alex@shopblocks.com>
2017-10-04 17:20:11 +0100

    Support for many files

--- src/lib.rs
+++ src/lib.rs
@@ -4,26 +4,26 @@ use regex::Regex;
 use std::io::{BufReader, BufRead};
 use std::fs::File;
 
-pub fn env(expression: &str, env_file: &str) -> Option<String> {
-    if let Ok(f) = File::open(env_file) {
-        let file = BufReader::new(f);
+pub fn env(expression: &str, env_files: &[&str]) -> Option<String> {
+    let mut result: Option<String> = None;
 
-        let regex = Regex::new(expression).unwrap();
+    for env_file in env_files {
+        if let Ok(f) = File::open(env_file) {
+            let file = BufReader::new(f);
 
-        let mut result: Option<String> = None;
+            let regex = Regex::new(expression).unwrap();
 
-        for line in file.lines() {
-            let l = line.unwrap();
+            for line in file.lines() {
+                let l = line.unwrap();
 
-            if regex.is_match(&l) {
-                for cap in regex.captures_iter(&l) {
-                    result = Some(cap[1].to_string());
+                if regex.is_match(&l) {
+                    for cap in regex.captures_iter(&l) {
+                        result = Some(cap[1].to_string());
+                    }
                 }
             }
         }
-
-        result
-    } else {
-        None
     }
+
+    result
 }
